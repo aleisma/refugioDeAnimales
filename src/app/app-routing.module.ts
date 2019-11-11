3515ce2220c2b44ff75425e4a57415952d225f31
aleisma <aleismabogado@gmail.com>
2019-11-11 10:23:37 -0300

    ruta de inicio agregada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+
+  { path: 'inicio', component: InicioComponent},   // I created start path
+  { path: '', redirectTo: '/inicio', pathMatch: 'full' }
+
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
