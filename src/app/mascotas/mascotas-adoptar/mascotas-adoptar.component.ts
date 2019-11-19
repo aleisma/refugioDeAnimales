9a08fd24265f08aac5e510c923cbd6eaed56b292
aleisma <aleismabogado@gmail.com>
2019-11-19 12:58:41 -0300

    agregando animaciones

--- src/app/mascotas/mascotas-adoptar/mascotas-adoptar.component.ts
+++ src/app/mascotas/mascotas-adoptar/mascotas-adoptar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MascotasService } from '../mascotas.service';
 import { Mascota } from '../mascota';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-mascotas-adoptar',
@@ -8,14 +9,31 @@ import { Mascota } from '../mascota';
   styleUrls: ['./mascotas-adoptar.component.css']
 })
 export class MascotasAdoptarComponent implements OnInit {
+  public mascotaAdoptada: boolean;
 
-  constructor(private mascotasService: MascotasService,) { }
+  constructor(private mascotasService: MascotasService) {
+
+    this.mascotaAdoptada = false;
+
+
+  }
+  public mascota: any = {};
   public mascotas: Mascota[] = [];
 
   ngOnInit() {
+
     this.mascotasService.getMascotas().subscribe(data => {
       this.mascotas = data;
     });
+  }
+
+  onChange( id: number ) {
+    this.mascotasService.getInfo(id).subscribe(data => {
+      this.mascota = data;
+      console.table(data);
+    });
+
+    this.mascotaAdoptada = true;
   }
 
 }
